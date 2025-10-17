d77b889541ec7746f7c2ecb3443d25ac836bd594
hugoaalderete7 <hugoaalderete7@gmail.com>
2025-10-17 13:29:43 -0300

    He creado la estructura básica del Proyecto, las carpetas components, page, hooks, store, slices y el store.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home_page/HomePage";
 
 function App() {
 
-
   return (
-    <>
-   
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
